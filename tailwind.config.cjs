6e43bd25a09fb9e0475740889929028119e69b11
Megan <meganhudson002@gmail.com>
2025-02-11 10:01:11 -0500

    landing page mock up and hero style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        moontime: ['MoonTime', 'cursive'],
+      },
+    },
+  },
   plugins: [require("@tailwindcss/typography"), require('daisyui')],
 };
